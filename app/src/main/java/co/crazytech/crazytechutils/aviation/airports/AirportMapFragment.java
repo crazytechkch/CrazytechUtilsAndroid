ac8c61834a30f962d54143d49b3934cd85732fb6
crazytechco <wymeric89@gmail.com>
2015-12-08 14:46:47 +0800

    201512081445

--- app/src/main/java/co/crazytech/crazytechutils/aviation/airports/AirportMapFragment.java
+++ app/src/main/java/co/crazytech/crazytechutils/aviation/airports/AirportMapFragment.java
@@ -3,6 +3,7 @@ package co.crazytech.crazytechutils.aviation.airports;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -70,6 +72,10 @@ public class AirportMapFragment extends MapWithSliderFragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) searchText.selectAll();
+                else {
+                    InputMethodManager imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+                }
             }
         });
         ImageButton btnSearch = (ImageButton)sliderView.findViewById(R.id.btn_search);
